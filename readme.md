04bc48569624ddd5c19a0ec02bb5f5a2e29ff7ea
Artemiy Garin <kvirair@gmail.com>
2013-11-13 19:34:55 +0400

    - Add yandex maps

--- readme.md
+++ readme.md
@@ -6,6 +6,6 @@ In this project i want identify pluses and minuses of most all popular maps SDK.
 
 Full article you may see here:
 
-<h2> I use your project and Google map not working, why? </h2>
+<h2> I make my own build, Google map not working, why? </h2>
 
 You forget add to settings.xml your key.
--- src/garin/artemiy/mapsexample/MapYandexActivity.java
+++ src/garin/artemiy/mapsexample/MapYandexActivity.java
@@ -1,10 +1,18 @@
 package garin.artemiy.mapsexample;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Author: Artemiy Garin
  * Date: 13.11.13
  */
 public class MapYandexActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_yandex_layout);
+    }
+
 }
