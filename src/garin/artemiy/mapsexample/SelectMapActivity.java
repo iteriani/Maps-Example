6502102094dfa95cd0cab195ef9878fbf01b989e
Artemiy Garin <kvirair@gmail.com>
2013-11-13 15:01:08 +0400

    - Init commit

---
+++ src/garin/artemiy/mapsexample/SelectMapActivity.java
@@ -0,0 +1,14 @@
+package garin.artemiy.mapsexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectMapActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+
+}
